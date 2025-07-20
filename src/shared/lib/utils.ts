fa003c46b771112aecbd6843d77a9fc212959827
Varadarajan-M <varad2k12@gmail.com>
2025-07-20 23:59:19 +0530

    feat(resume builder): added pdf download

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -14,3 +14,12 @@ export const kebabToPascalCase = (slug: string): string => {
     ?.map((part) => part?.charAt(0)?.toUpperCase() + part?.slice(1))
     ?.join("");
 };
+
+export const downloadFile = (url: string, fileName: string) => {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
