d6791547da6d27becca5479939064fdd3e426ba1
Varadarajan-M <varad2k12@gmail.com>
2025-10-27 22:58:38 +0530

    fix: disable posthog experimentally

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -1,13 +1,13 @@
-import posthog from 'posthog-js';
+// import posthog from 'posthog-js';
 
-const isDev = process.env.NODE_ENV === 'development';
+// const isDev = process.env.NODE_ENV === 'development';
 
-if (
-  !isDev &&
-  process.env.NEXT_PUBLIC_POSTHOG_KEY &&
-  process.env.NEXT_PUBLIC_POSTHOG_HOST
-)
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    defaults: '2025-05-24',
-  });
+// if (
+//   !isDev &&
+//   process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+//   process.env.NEXT_PUBLIC_POSTHOG_HOST
+// )
+//   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+//     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+//     defaults: '2025-05-24',
+//   });
